4b4efe5dc781bff18af700727d04eb3bbad5c33c
moiseshilario <moiseshilariu@gmail.com>
2017-08-08 14:06:03 -0300

    Create separator component (list)

---
+++ separator.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { View } from 'react-native'
+
+const Separator = () => (
+  <View style={{
+    height: 0.5,
+    backgroundColor: '#C4C4C4',
+    flex: 1,
+    marginLeft: 15,
+    marginRight: 15
+  }}
+  />
+)
+
+export default Separator
