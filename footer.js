4d0a263f77b00ca67f5ec97d9b35cc4b87d78351
moiseshilario <moiseshilariu@gmail.com>
2017-08-08 16:07:44 -0300

    Implement footer component

---
+++ footer.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native'
+
+const Footer = ({ filter, onFilter }) => (
+  <View style={styles.container}>
+    <View style={styles.filters} >
+      <TouchableOpacity
+        style={[styles.filter, filter === 'ALL' && styles.selected]}
+        onPress={() => onFilter('ALL')}
+      >
+        <Text>All</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.filter, filter === 'ACTIVE' && styles.selected]}
+        onPress={() => onFilter('ACTIVE')}
+      >
+        <Text>Active</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.filter, filter === 'COMPLETED' && styles.selected]}
+        onPress={() => onFilter('COMPLETED')}
+      >
+        <Text>Completed</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16
+    // flexDirection: 'row',
+    // alignItems: 'center',
+    // justifyContent: 'space-between'
+  },
+  filters: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  filter: {
+    padding: 8,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'transparent'
+  },
+  selected: {
+    borderColor: 'rgba(175, 47, 47, .2)'
+  }
+})
+
+export default Footer
