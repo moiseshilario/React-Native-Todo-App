bf003bc5e902948d7778d3b0e4e3dd7a6d8b1522
moiseshilario <moiseshilariu@gmail.com>
2017-08-08 14:07:11 -0300

    Create header component (TextInput)

---
+++ header.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import {
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  StyleSheet
+} from 'react-native'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <View style={styles.header}>
+        <TouchableOpacity
+          onPress={this.props.onToggleAllComplete}
+        >
+          <Text style={styles.toggleIcon}>
+            {String.fromCharCode(10003)}
+          </Text>
+        </TouchableOpacity>
+        <TextInput
+          value={this.props.value}
+          onChangeText={this.props.onChange}
+          onSubmitEditing={this.props.onAddItem}
+          blurOnSubmit={false}
+          placeholder="What needs to be done?"
+          returnKeyType="done"
+          style={styles.input}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  header: {
+    paddingHorizontal: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  toggleIcon: {
+    fontSize: 30,
+    color: '#CCC'
+  },
+  input: {
+    flex: 1,
+    height: 50,
+    marginLeft: 16
+  }
+})
